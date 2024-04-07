d0a5dde1c2be960636771ed52cea7bd0b63bca47
Adam Jurčišin <jurcisin.adam@gmail.com>
2024-04-07 22:24:54 +0200

    Z4

--- dbs_assignment_jurcisin/router.py
+++ dbs_assignment_jurcisin/router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from dbs_assignment_jurcisin.endpoints import version
+from dbs_assignment_jurcisin.endpoints.z1 import version
 
-from dbs_assignment_jurcisin.endpoints import posts
-from dbs_assignment_jurcisin.endpoints import tags
-from dbs_assignment_jurcisin.endpoints import users
-from dbs_assignment_jurcisin.endpoints import posts4_5
+from dbs_assignment_jurcisin.endpoints.z2 import posts
+from dbs_assignment_jurcisin.endpoints.z2 import tags
+from dbs_assignment_jurcisin.endpoints.z2 import users
+from dbs_assignment_jurcisin.endpoints.z2 import posts4_5
 
 from dbs_assignment_jurcisin.endpoints.z3 import task_1
 from dbs_assignment_jurcisin.endpoints.z3 import task_2
@@ -15,6 +15,7 @@ from dbs_assignment_jurcisin.endpoints.z3 import task_4
 
 router = APIRouter()
 router.include_router(version.router, tags=["version"])
+
 router.include_router(posts.router, tags=["posts"])
 router.include_router(users.router, tags=["users"])
 router.include_router(tags.router, tags=["tags"])
